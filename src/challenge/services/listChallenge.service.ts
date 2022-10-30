fa453dd35268e3132058aab275b9811ddd1161a3
repifanio <rafael.cepifanio@gmail.com>
2022-10-29 22:46:23 -0300

    include requirements in list challenges

--- src/challenge/services/listChallenge.service.ts
+++ src/challenge/services/listChallenge.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Challenge } from '../../shared/entities/challenge.entity';
+import { ListChallengeDto } from '../dto/listChallenge.dto';
+import { ConvertQueryResultToJson } from '../../shared/services/convertQueryResult.service';
 
 @Injectable()
 export class ListChallenge {
@@ -10,9 +12,32 @@ export class ListChallenge {
     private challengeRepository: Repository<Challenge>,
   ) {}
 
-  async run(): Promise<Challenge[]> {
+  async run(): Promise<ListChallengeDto[]> {
+    const convertQueryResult = new ConvertQueryResultToJson();
+    const allChallenges: ListChallengeDto[] = [];
+
     try {
-      return await this.challengeRepository.findBy({ isActive: true });
+      let challengeToReturn: ListChallengeDto;
+
+      const challengesFormatted: ListChallengeDto[] = convertQueryResult.run(
+        await this.challengeRepository.query(
+          `select c.id, c.challengeName, c.startAt, c.endAt from challenge c`,
+        ),
+      );
+
+      for (const challenge of challengesFormatted) {
+        challengeToReturn = challenge;
+
+        challengeToReturn.requirements = convertQueryResult.run(
+          await this.challengeRepository.query(
+            `Select r.id, r.requirement, cr.value from challenge_requirements cr, requirements r where cr.requirementsId = r.id and cr.challengeId = ${challenge.id}`,
+          ),
+        );
+
+        allChallenges.push(challengeToReturn);
+      }
+
+      return allChallenges;
     } catch (err) {
       return err.code;
     }
